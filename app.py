0df0cc4ebf2565b0279a6c516c2998ad58441b8f
Riya Kushwaha <riyakushwaha1125@gmail.com>
2025-06-06 18:31:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,49 +23,57 @@ if not st.session_state["authenticated"]:
     login()
 else:
         
-    # --- Revenue Prediction App ---
-    st.title("Revenue Prediction App")
+    # --- Loan Approval Prediction App ---
+    st.title("Loan Approval Prediction App")
     st.header("Enter Feature Values")
         
     # Input fields for each feature
-    Credit_Score = st.number_input("Credit Score",start = 400 ,step= 20)
-    Total_TL = st.number_input("Total TL", step=0.1)
-    Age_Oldest_TL = st.number_input("Age of Oldest TL", step=0.1)
-    num_std_6mts = st.number_input("Number of Standard Accounts in 6M", step=0.1)
-    num_std_12mts = st.number_input("Number of Standard Accounts in 12M", step=0.1)
-    max_recent_level_of_deliq =st.number_input("Maximum level of delinquency in recent times", step=0.1)
-    Tot_Closed_TL = st.number_input("Total Closed TL", step=0.1)
-    num_std = st.number_input("Number of standard accounts", step=0.1)
-    enq_L12m = st.number_input("Total enquires in Last 12 months", step=0.1)
-    tot_enq = st.number_input("Total Enquiry", step=0.1) 
-    PL_enq = st.number_input("Personal Loan Enquiry", step=0.1)
-    PROSPECTID = st.number_input("Applicant ID", step=0.1)
-    PL_enq_L12m = st.number_input("Personal Loan enquiry in Last 12 months", step=0.1)
-    enq_L6m = st.number_input("Enquiry in last 6 months", step=0.1)
-    Secured_TL = st.number_input("Secured TL", step=0.1)
-    time_since_recent_enq = st.number_input("Days since the most recent enquiry", step=0.1)
+    Credit_Score = st.number_input("Credit Score",start = 400 ,step= 1)
+    Total_TL = st.number_input("Total TL", step= 1)
+    Age_Oldest_TL = st.number_input("Age of Oldest TL", step= 1)
+    num_std_6mts = st.number_input("Number of Standard Accounts in 6M", step= 1)
+    num_std_12mts = st.number_input("Number of Standard Accounts in 12M", step= 1)
+    max_recent_level_of_deliq =st.number_input("Maximum level of delinquency in recent times", step= 1)
+    Tot_Closed_TL = st.number_input("Total Closed TL", step= 1)
+    num_std = st.number_input("Number of standard accounts", step= 1)
+    enq_L12m = st.number_input("Total enquires in Last 12 months", step= 1)
+    tot_enq = st.number_input("Total Enquiry", step= 1) 
+    PL_enq = st.number_input("Personal Loan Enquiry", step= 1)
+    PROSPECTID = st.number_input("Applicant ID", step= 1)
+    PL_enq_L12m = st.number_input("Personal Loan enquiry in Last 12 months", step= 1)
+    enq_L6m = st.number_input("Enquiry in last 6 months", step= 1)
+    Secured_TL = st.number_input("Secured TL", step= 1)
+    time_since_recent_enq = st.number_input("Days since the most recent enquiry", step= 1)
   
 
     # Collect input in a DataFrame
     input_data = pd.DataFrame([{
-        "Land Class": land_class,
-        "Land Category": land_category,
-        "State": state,
-        "Revenue Type": revenue_type,
-        "Mineral Lease Type": lease_type,
-        "Commodity": commodity,
-        "County": county,
-        "Product": product
+        "Credit Score": Credit_Score,
+        "Total TL": Total_TL,
+        "Age of Oldest TL": Age_Oldest_TL,
+        "Number of Standard Accounts in 6M": num_std_6mts,
+        "Number of Standard Accounts in 12M": num_std_12mts,
+        "Maximum level of delinquency in recent times": max_recent_level_of_deliq,
+        "Total Closed TL": Tot_Closed_TL,
+        "Number of standard accounts": num_std,
+        "Total enquires in Last 12 months": enq_L12m,
+        "Total Enquiry": tot_enq,
+        "Personal Loan Enquiry": PL_enq,
+        "Applicant ID": PROSPECTID,
+        "Personal Loan enquiry in Last 12 months": PL_enq_L12m,
+        "Enquiries in last 6 months": enq_L6m,
+        "Secured TL": Secured_TL,
+        "Days since the most recent enquiry": time_since_recent_enq
     }])
 
-    # Load the CatBoost model  
+    # Load the  model  
     with open("Model.pkl", "rb") as f:
          model = pickle.load(f)
 
     # Predict and display result
-    if st.button("Predict Revenue"):
+    if st.button("Loan Approval"):
         prediction =  model.predict(input_data)
-        st.success(f"Estimated Revenue: ${prediction[0]:,.2f}")
+        st.success(f"Estimated Approval: ${prediction[0]:,.2f}")
 
         st.markdown("""
         <hr>
