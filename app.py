b3af33b4688e3003d495969ecdd7ed947da7fb11
Riya Kushwaha <riyakushwaha1125@gmail.com>
2025-06-06 18:34:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ else:
     }])
 
     # Load the  model  
-    with open("Model.pkl", "rb") as f:
+    with open("model.pkl", "rb") as f:
          model = pickle.load(f)
 
     # Predict and display result
