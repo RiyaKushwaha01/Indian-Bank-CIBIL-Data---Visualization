cf5944a691941b4e63eee2069ed3a5de8e8623e8
Riya Kushwaha <riyakushwaha1125@gmail.com>
2025-06-06 18:08:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,23 +28,23 @@ else:
     st.header("Enter Feature Values")
         
     # Input fields for each feature
-    Credit_Score = st.selectbox("Credit Score", ["Federal", "Native American"])
-    Total_TL = st.selectbox("Total TL", ["Onshore", "Offshore", "Not Tied to a Lease"])
-    Age_Oldest_TL = st.selectbox("Age of Oldest TL", ["Texas","Missouri", "Alaska", "California", "Georgia", "New York", "New Mexico", "Indiana", "Florida", "Washington"])
-    enq_L3m = st.selectbox(step = +1 (42))
-    num_std_6mts = st.selectbox("Mineral Lease Type", [ "Hardrock","Limestone", "Gold", "Coal", "Silver", "Oil & Gas", "Sulfur", "Gilsonite", "Gypsum", "Sodium", "Phosphate", "Gemstones"])
-    num_std_12mts = st.selectbox("Commodity", ["Oil", "Gas", "Coal", "Copper", "Hardrock", "Natural gas liquids", "Gilsonite", "Phosphate", "Oil & gas (pre-production)", "Geothermal"])
-    max_recent_level_of_deliq = st.selectbox(step = +1(900))
-    Tot_Closed_TL = st.selectbox("Product", ["Nitrogen", "Copper Concentrate","Oil", "Coal Bed Methane", "Coal", "Gas Plant Products", "Calcium Oxide", "Carbon Dioxide Gas (CO2)", "Fuel Gas", "Fuel Oil", "Helium"])
-    num_std = 
-    enq_L12m = st.selectbox(step = +1 (42))
-    tot_enq = 
-    PL_enq = 
-    PROSPECTID = 
-    PL_enq_L12m = 
-    enq_L6m = 
-    Secured_TL = 
-    time_since_recent_enq = 
+    Credit_Score = st.number_input("Inventory", step=0.1)
+    Total_TL = st.number_input("Inventory", step=0.1)
+    Age_Oldest_TL = st.number_input("Inventory", step=0.1)
+    num_std_6mts = st.number_input("Inventory", step=0.1)
+    num_std_12mts = st.number_input("Inventory", step=0.1)
+    max_recent_level_of_deliq =st.number_input("Inventory", step=0.1)
+    Tot_Closed_TL = st.number_input("Inventory", step=0.1)
+    num_std = st.number_input("Inventory", step=0.1)
+    enq_L12m = st.number_input("Inventory", step=0.1)
+    tot_enq = st.number_input("Inventory", step=0.1) 
+    PL_enq = st.number_input("Inventory", step=0.1)
+    PROSPECTID = st.number_input("Inventory", step=0.1)
+    PL_enq_L12m = st.number_input("Inventory", step=0.1)
+    enq_L6m = st.number_input("Inventory", step=0.1)
+    Secured_TL = st.number_input("Inventory", step=0.1)
+    time_since_recent_enq = st.number_input("Inventory", step=0.1)
+  
 
     # Collect input in a DataFrame
     input_data = pd.DataFrame([{
