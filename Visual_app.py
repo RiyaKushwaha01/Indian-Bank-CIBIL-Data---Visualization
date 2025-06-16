f1fab08a4593c7517e2b27aa54f919d3575e40b1
Riya Kushwaha <riyakushwaha1125@gmail.com>
2025-06-16 19:00:31 +0530

    Update Visual_app.py

--- Visual_app.py
+++ Visual_app.py
@@ -88,7 +88,7 @@ else:
     gender_data = filtered_df2["GENDER"].value_counts()
     # Use a slightly larger figure for clarity, but render tightly
     fig2, ax2 = plt.subplots(figsize=(2, 2), dpi=200)  # High DPI for sharpness
-    ax2.pie(gender_data,labels=gender_data.index,autopct="%.2f%%",startangle=90,textprops={'fontsize': 6}  # Small but readable)
+    ax2.pie(gender_data,labels=gender_data.index,autopct="%.2f%%",startangle=90,textprops={'fontsize': 6})  # Small but readable
     # Equal aspect ratio ensures a circle
     ax2.axis("equal")
     st.pyplot(fig2, bbox_inches="tight")
